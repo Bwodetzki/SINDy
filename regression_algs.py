efe5c5b07777df019fe68d67d083439c5cf3b1b0
Bwodetzki <50225476+Bwodetzki@users.noreply.github.com>
2024-04-03 21:37:38 -0400

    Make Cross Validation Algs - SR3 cross validation function - proximal optimization cross validation function

--- regression_algs.py
+++ regression_algs.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from data_generator import general_func
+from tqdm import tqdm
 
 # def feature_generator(num_states=1, polynomials)
-
+'''
+Proximal Operators
+'''
 def l1_prox_op(z, prox_w):
     return jnp.sign(z) * jnp.maximum(abs(z) - prox_w, 0)
 
@@ -17,6 +20,9 @@ def l0_prox_op(z, prox_w):
         z_updated.append(val)
     return jnp.array(z_updated)
 
+'''
+Core Proximal Optimization
+'''
 def proximal_optimization(Phi, b, prox_op, alpha, x_init, eps=1e-8, prox_w=0.1):
     x = x_init
     x_old = x_init + 1.
@@ -25,15 +31,20 @@ def proximal_optimization(Phi, b, prox_op, alpha, x_init, eps=1e-8, prox_w=0.1):
     max_its = 10_000
     while (jnp.linalg.norm(x - x_old) > eps) and (i<max_its):
         x_old = x
-        # z = x - alpha*Phi.T@(Phi@x - b)
-        z = jnp.linalg.lstsq(Phi, b)[0]
-        x = prox_op(z, alpha, alpha*prox_w/2)
+        z = x - alpha*Phi.T@(Phi@x - b)
+        # z = jnp.linalg.lstsq(Phi, b)[0]
+        x = prox_op(z, alpha*prox_w/2)
         i+=1
+        if any(jnp.isnan(x)):
+            print('here1')
     if i>=max_its:
         print('here')
     return x
 
-def SR3(Phi, b, C, prox_op, x_init, kappa=1, prox_w=0.1, eps=1e-8):
+'''
+SR3 Optimization, Implemented Acoording To: https://uw-amo.github.io/AMO_Site/software/sr3/
+'''
+def SR3(Phi, b, C, prox_op, x_init, kappa=1, prox_w=0.1, eps=1e-6):
     x = x_init
     w = jnp.zeros_like(x)
     w_old = w + 1.
@@ -52,11 +63,19 @@ def SR3(Phi, b, C, prox_op, x_init, kappa=1, prox_w=0.1, eps=1e-8):
     if i>=max_its:
         print('here')
     return (w, x)
-
+# Proximal variant of SR3, as introduced in the SR3 paper
 def SR3_proximal(Phi, b, C, prox_op, x_init, kappa=1, prox_w=0.1, eps=1e-8):
     pass
 
+'''
+Original SINDy Regression
+'''
 
+
+
+'''
+Cross Validation for Optimization Algorithms
+'''
 def cross_validation(optimization_func, features, train_data, test_data, param_ranges, resolution=100):
     '''
     optimization must be a function that takes 3 input parameters
@@ -64,7 +83,7 @@ def cross_validation(optimization_func, features, train_data, test_data, param_r
     solutions = []
     errors = []
     params = jnp.linspace(param_ranges[0], param_ranges[1], resolution)
-    for param in params:
+    for param in tqdm(params):
         sol = optimization_func(features(train_data[0]), train_data[1], param)
         sol = sol.flatten()
         solutions.append(sol)
@@ -75,44 +94,76 @@ def cross_validation(optimization_func, features, train_data, test_data, param_r
 
     return (solutions[idx], errors[idx], params[idx])
 
+def proximal_CV(X, y, features, alpha, key, proximal_operator=l1_prox_op, test_size=0.25, param_ranges=[0.001, 10], resolution=1000):
+    key, subkey = jax.random.split(key)
+    size = len(features(0))
+    x_init = jax.random.normal(subkey, (size,))
+
+    key, subkey = jax.random.split(key)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=int(subkey[0]))
+    opt_fun = lambda phi_mat, data, param: proximal_optimization(phi_mat, data, proximal_operator, alpha, x_init, prox_w=param)
+    fit, error, prox_w = cross_validation(opt_fun, features, 
+                                            train_data=(X_train, y_train), 
+                                            test_data=(X_test, y_test), 
+                                            param_ranges=param_ranges, 
+                                            resolution=resolution)
+    return fit, error, prox_w
+
+def SR3_CV(X, y, features, C, kappa, key, proximal_operator=l1_prox_op, test_size=0.25, param_ranges=[0.001, 10], resolution=1000):
+    key, subkey = jax.random.split(key)
+    size = len(features(0))
+    x_init = jax.random.normal(subkey, (size,))
+
+    key, subkey = jax.random.split(key)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=int(subkey[0]))
+    opt_fun = lambda phi_mat, data, param: SR3(phi_mat, data, C, proximal_operator, x_init, kappa=kappa, prox_w=param)[0]
+    fit, error, prox_w = cross_validation(opt_fun, features, 
+                                            train_data=(X_train, y_train), 
+                                            test_data=(X_test, y_test), 
+                                            param_ranges=param_ranges, 
+                                            resolution=resolution)
+    return fit, error, prox_w
+
 def main():
-   key, subkey = jax.random.split(jax.random.PRNGKey(0))
-   fun = lambda x: 3*x**3 - x**2 - 2*x + 3
-   x, y = general_func(fun, subkey, lim=[-2.5, 2.5], num_samples=1000, sigma=1)
+    key, subkey = jax.random.split(jax.random.PRNGKey(0))
+    fun = lambda x: 3*x**3 - x**2 - 2*x + 3
+    x, y = general_func(fun, subkey, lim=[-2.5, 2.5], num_samples=1000, sigma=1)
 
-#    poly = PolynomialFeatures(degree=5)
-#    Phi = poly.fit_transform(x)
-   size = 8
-   features = lambda x: jnp.array([x**i for i in range(size)]).T
-   Phi = features(x)
+    #    poly = PolynomialFeatures(degree=5)
+    #    Phi = poly.fit_transform(x)
+    size = 5
+    features = lambda x: jnp.array([x**i for i in range(size)]).T
+    Phi = features(x)
 
-   key, subkey = jax.random.split(key)
-   x_init = jax.random.normal(subkey, (size,))
+    key, subkey = jax.random.split(key)
+    x_init = jax.random.normal(subkey, (size,))
 
-#    fit = proximal_optimization(Phi, y, l1_prox_op, 0.0001, x_init, prox_w=0.3)
-   
-   C = jnp.eye(size)
-   fit, _ = SR3(Phi, y, C, l1_prox_op, x_init, kappa=1, prox_w=0.1)
-#    print(fit)
+    # fit = proximal_optimization(Phi, y, l1_prox_op, 0.00001, x_init, prox_w=3)
 
-   ## Cross Validation
-   key, subkey = jax.random.split(key)
-   x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=int(subkey[0]))
-   opt_fun = lambda phi_mat, data, param: SR3(phi_mat, data, C, l0_prox_op, x_init, kappa=1, prox_w=param)[0]
-   fit, error, prox_w = cross_validation(opt_fun, features, 
-                                         train_data=(x_train, y_train), 
-                                         test_data=(x_test, y_test), 
-                                         param_ranges=[0.001, 10], 
-                                         resolution=1000)
-   print(error)
-   print(fit)
+    C = jnp.eye(size)
+    # fit, _ = SR3(Phi, y, C, l1_prox_op, x_init, kappa=1, prox_w=0.1)
+    #    print(fit)
 
-#    fit = jnp.linalg.lstsq(Phi, y)[0]
+    ## Cross Validation
+    key, subkey = jax.random.split(key)
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=int(subkey[0]))
+    # opt_fun = lambda phi_mat, data, param: SR3(phi_mat, data, C, l0_prox_op, x_init, kappa=1, prox_w=param)[0]
+    # fit, error, prox_w = cross_validation(opt_fun, features, 
+    #                                         train_data=(x_train, y_train), 
+    #                                         test_data=(x_test, y_test), 
+    #                                         param_ranges=[0.001, 10], 
+    #                                         resolution=1000)
+    
+    fit, error, prox_w = proximal_CV(x, y, features, alpha=0.00001, key=subkey, param_ranges=[0.001, 100], resolution=100)
+    # print(error)
+    print(fit)
 
-   plt.figure()
-   plt.plot(x, y)
-   plt.plot(x, Phi@fit)
-   plt.show()
+    #    fit = jnp.linalg.lstsq(Phi, y)[0]
+
+    plt.figure()
+    plt.plot(x, y)
+    plt.plot(x, Phi@fit)
+    plt.show()
 
 if __name__ == "__main__":
-    main()
+    main() 
